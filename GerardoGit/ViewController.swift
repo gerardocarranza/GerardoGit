bcc55abab4c6787af75e42a8916e7d8e40357f6c
Gerardo Carranza <geraljrluna@gmail.com>
2019-10-29 13:08:23 -0400

    added outlets and action

--- GerardoGit/ViewController.swift
+++ GerardoGit/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var testLabelOne: UILabel!
+    
+    @IBOutlet weak var testLabelTwo: UILabel!
+    
+    @IBOutlet weak var testLabelThree: UILabel!
+    
+    @IBOutlet weak var testLabelFour: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
 }
 
